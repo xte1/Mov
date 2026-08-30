951c0ce674b3ee5013b7858948f09c727f75ed44
dev <anwar.nille@gmail.com>
2026-08-31 01:44:58 +0300

    Update ContentView.swift

--- iMOV/ContentView.swift
+++ iMOV/ContentView.swift
@@ -1,16 +1,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var tmdbService = TMDBService()
+    @StateObject var tmdbService = TMDBService()
     
     var body: some View {
         NavigationView {
             List(tmdbService.movies) { movie in
                 NavigationLink(destination: MovieDetailView(movie: movie)) {
                     VStack(alignment: .leading, spacing: 4) {
-                        Text(movie.title)
+                        Text(movie.title ?? "بدون عنوان")
                             .font(.headline)
-                        Text(movie.overview)
+                        Text(movie.overview ?? "لا توجد تفاصيل")
                             .font(.subheadline)
                             .foregroundColor(.secondary)
                             .lineLimit(1)
